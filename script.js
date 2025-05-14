5b279cbd7f766a678d44790a5ccc86c6d243b360
Julius <188955756+4483-s@users.noreply.github.com>
2025-05-14 10:54:41 +0800

    saved changes

---
+++ script.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "already read" : "not read yet"
+    }`;
+  };
+}
+
+const Demian = new Book("Demian", "unknown", 255, true);
+console.log(Demian.info());
